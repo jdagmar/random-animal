63bbfe24af1457f0271ad18ba6bae686879abb2d
Jessica Gustavsson <dagmar.j.gustavsson@gmail.com>
2020-04-10 10:10:17 +0200

    Remove unused styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MainHeading = styled.h1`
-  // background-color: #fa8572;
-  // color: #ffffff;
   padding: 0.5rem 1rem;
   font-family: 'Sen', sans-serif;
   font-size: 1.2rem;
 `;
 
 const Preamble = styled.p`
-  // background-color: #b24968;
-  // color: #ffffff;
   padding: 0.5rem 1rem;
   margin: 0.5rem;
   font-family: 'Roboto', sans-serif;
